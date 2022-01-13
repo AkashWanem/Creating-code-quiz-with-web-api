b1b155e8c3f0f46332653152b73c004943b5ccc7
Akash Limbu <akashwanem@hotmail.com>
2022-01-13 23:36:08 +0000

    written all the question & answers

---
+++ assets/javascript/questions.js
@@ -0,0 +1,34 @@
+// all the questions for quiz goes here.
+
+var questions = [
+    {
+        question: "console.log(typeof false); What will show in the console?",
+        choices: ["number", "null", "undefined", "boolean"],
+        answer: "boolean"
+    },
+    {
+        question: "The x += y is the same as:",
+        choices: ["y = x + y", "x = y + y", "y = x + x", "x = x + y"],
+        answer: "x = x + y"
+    },
+    {
+        question: "Which of the following is correct about features of JavaScript?",
+        choices: [
+            "JavaScript is is complementary to and integrated with HTML.",
+            "JavaScript is open and cross-platform.",
+            "Both of the above.",
+            "All of the above."
+        ],
+        answer: "Both of the above"
+    },
+    {
+        question: "Which built-in method removes the last element from an array and returns that element?",
+        choices: ["last()", "get()", "pop()", "None of the above"],
+        answer: "pop()"
+    },
+    {
+        question: "Which of the following function of Number object returns a string value version of the current number?",
+        choices: ["toString()", "toFixed()", "toLocaleString()", "toPrecision()"],
+        answer: "toString()"
+    }
+];
